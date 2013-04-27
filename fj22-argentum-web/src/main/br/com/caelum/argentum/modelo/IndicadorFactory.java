e11e44676a318b7f3ba9ddbc61fed71c7a59f856
ericvinicius <eric.camargo@gmail.com>
2013-04-27 16:12:01 -0300

    curso caelum fj22

---
+++ fj22-argentum-web/src/main/br/com/caelum/argentum/modelo/IndicadorFactory.java
@@ -0,0 +1,14 @@
+package br.com.caelum.argentum.modelo;
+
+public class IndicadorFactory {
+	private String nomeMedia;
+	private String nomeIndicador;
+	
+	public IndicadorFactory(String nomeIndicador, String nomeMedia) {
+		super();
+		this.nomeIndicador = nomeIndicador;
+		this.nomeMedia = nomeMedia;
+	}
+	
+	
+}
